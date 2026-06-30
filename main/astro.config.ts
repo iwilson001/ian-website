1f25255deb79a260208ccccf1d185bc0a5cef0c8
ian <ian@ianmwilson.com>
2026-06-29 19:51:24 -0500

    continues building out main website files and infra

--- main/astro.config.ts
+++ main/astro.config.ts
@@ -1,15 +1,11 @@
-import { defineConfig } from "astro/config";
-import viteTscPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    output: "static",
+    output: 'static',
     integrations: [],
-    server: { port: 8000, host: "0.0.0.0" },
-    trailingSlash: "ignore",
-    vite: {
-        plugins: [viteTscPaths()],
-        build: { sourcemap: false },
-    },
-    prefetch: { defaultStrategy: "load", prefetchAll: true },
+    server: { port: 8000, host: '0.0.0.0' },
+    trailingSlash: 'ignore',
+    vite: { plugins: [], build: { sourcemap: false }, resolve: { tsconfigPaths: true } },
+    prefetch: { defaultStrategy: 'load', prefetchAll: true },
     devToolbar: { enabled: false },
 });
